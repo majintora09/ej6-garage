42d6a0ff6018eae346e0a736987ae908b412d644
majintora09 <majintora@hotmail.com>
2026-05-11 14:00:50 +0200

    fix layout error

--- resources/views/resources/views/garage.blade.php
+++ resources/views/resources/views/garage.blade.php
@@ -1,30 +1,8 @@
 @extends('layout')
 
 @section('content')
-
-    <div class="grid">
-        <div class="card">
-            <h2>Car Info</h2>
-            <p><strong>Model:</strong> Honda Civic EJ6</p>
-            <p><strong>Year:</strong> 1997</p>
-            <p><strong>Color Code:</strong> G-82P-5</p>
-            <p><strong>Interior:</strong> TYPE-K, DK.GRAY</p>
-        </div>
-
-        <div class="card">
-            <h2>Current Build Vibe</h2>
-            <p>Dark green street/JDM style with a subtle Majin-inspired theme.</p>
-        </div>
-
-        <div class="card">
-            <h2>Current Priorities</h2>
-            <ul>
-                <li>Fix exhaust alignment</li>
-                <li>Check oil leak</li>
-                <li>Repair rust spots</li>
-                <li>Improve front bumper/headlight alignment</li>
-            </ul>
-        </div>
+    <div class="card">
+        <h2>EJ6 Garage Works</h2>
+        <p>If you see this, the layout is working.</p>
     </div>
-
 @endsection
