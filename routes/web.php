d42c3f55a9fdd05c3a6387e765cbf542701f763a
majintora09 <majintora@hotmail.com>
2026-05-11 17:06:06 +0200

    pray

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,18 @@ Route::get('/gallery', function () {
 Route::get('/calculator', function () {
     return view('calculator');
 });
+
+Route::get('/debug-db', function () {
+    return response()->json([
+        'config_default' => config('database.default'),
+        'env_DB_CONNECTION' => env('DB_CONNECTION'),
+        'env_DB_HOST' => env('DB_HOST'),
+        'env_DB_PORT' => env('DB_PORT'),
+        'env_DB_DATABASE' => env('DB_DATABASE'),
+        'env_DB_USERNAME' => env('DB_USERNAME'),
+        'env_MYSQLHOST' => env('MYSQLHOST'),
+        'env_MYSQLPORT' => env('MYSQLPORT'),
+        'env_MYSQLDATABASE' => env('MYSQLDATABASE'),
+        'env_MYSQLUSER' => env('MYSQLUSER'),
+    ]);
+});
