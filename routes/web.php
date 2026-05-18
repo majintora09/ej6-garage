3c14c62218fa9a37c9675963beea4fc71e6fd34e
majintora09 <majintora@hotmail.com>
2026-05-18 05:26:08 +0200

    fix mods page

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,9 @@ Route::get('/maintenance', [MaintenanceController::class, 'index']);
 Route::post('/maintenance', [MaintenanceController::class, 'store']);
 Route::delete('/maintenance/{maintenance}', [MaintenanceController::class, 'destroy']);
 
-Route::get('/mods', function () {
-    Route::get('/mods', [ModController::class, 'index']);
-    Route::post('/mods', [ModController::class, 'store']);
-    Route::delete('/mods/{mod}', [ModController::class, 'destroy']);
-});
+Route::get('/mods', [ModController::class, 'index']);
+Route::post('/mods', [ModController::class, 'store']);
+Route::delete('/mods/{mod}', [ModController::class, 'destroy']);
 
 Route::get('/parts', function () {
     return view('parts');
